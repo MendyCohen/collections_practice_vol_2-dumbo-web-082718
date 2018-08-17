c1e8f3a91acd7d2238179c971582b767be9bffae
Mendy <mendybeltcohen@gmail.com>
2018-08-17 16:25:05 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,27 @@
-# your code goes here
+def contain_a(words_arr)
+  words_arr.select {|word| word.include?("a")}
+end
+
+def begins_with_r(word_arr)
+  word_arr.all? {|word| word.start_with?("r")}
+end
+
+def first_wa(word_arr)
+  word_arr.find {|word| word.to_s.start_with?("wa")}
+end
+
+def remove_non_strings(str_array)
+  str_array.delete_if { |non_str| !(non_str.is_a? String) }
+end
+
+def count_elements(names_arr)
+  tracker = Hash.new
+  names_arr.each do |name|
+    if tracker[name] == nil
+      tracker[name] = 1
+    else
+      tracker[name] +=1
+    end
+  end
+    tracker.map {|key, value| {:name => key, :count => value}}
+end
